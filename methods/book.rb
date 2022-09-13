dd948a93b44757f59d3a640d9a8f1dfe38d86952
Ulises Largaespada <ulisesedu7@users.noreply.github.com>
2022-09-12 21:20:17 -0600

    Set up book class with its parameters and methods

--- methods/book.rb
+++ methods/book.rb
@@ -1,8 +1,18 @@
 require_relative './item'
 
 class Book < Item
-  def initialize
+  attr_accessor :publisher, :cover_state
 
+  def initialize(publish_date, publisher, cover_state, id = Random.rand(1..10_000), archived: false)
+    super(publish_date, id, archived)
+    @publisher = publisher
+    @cover_state = cover_state
+  end
+
+  def can_be_archived?
+    return true if super || cover_state == 'bad'
+
+    false
   end
 
 
