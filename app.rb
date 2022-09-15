41efcaac9c381a7717e5dc0d17bfda8b3cdf6792
Ulises Largaespada <ulisesedu7@users.noreply.github.com>
2022-09-14 23:01:57 -0600

    Finish save and load labels methods

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require_relative './methods/label'
 require_relative './app-methods/books_actions'
 require_relative './app-methods/label_actions'
 require_relative './data-functions/book_data'
+require_relative './data-functions/label_data'
 
 class App
   attr_accessor :books, :labels 
@@ -17,10 +18,12 @@ class App
 
   def save_data
     save_books(@books)
+    save_labels(@labels)
   end
 
   def load_data
     @books = load_books
+    @labels = load_labels
   end
 
 end
